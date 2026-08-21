b571309515873827fa8d7f06fe3bda3bcdfe2331
Most-05 <moszmgamez@gmail.com>
2026-08-22 01:52:21 +0700

    รวมทุกหน้าเข้าด้วยกันผ่านระบบ route
    ประกาศ route ของทุกหน้าไว้ใน App.js ครอบด้วย BalanceProvider
    เพื่อให้ยอดเงินใช้ร่วมกันได้ทั้งเว็บ และวาง Header ไว้เหนือ Routes
    พร้อมสไตล์ส่วนกลางของแอป

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './pages/Home';
+import Game from './pages/Gamestore';
+import AddFunds from './pages/AddFunds';
+import { BalanceProvider } from './BalanceContext';
 import './App.css';
+import ARKHome from './pages/ARKHome';
+import DinoPackPage from './pages/DinoPackPage';
+import BlueprintPage from './pages/BlueprintPage';
+import EquipmentPage from './pages/EquipmentPage';
+import StructuresPage from './pages/StructuresPage ';
+import ArtifactsPage from './pages/ArtifactsPage';
+import CavePage from './pages/CavePage';
+import CheckoutPage from './pages/CheckoutPage';
+import ROVHome from './pages/ROVHome'
+import ROVShop from './pages/ROVShop';
+import FortnightHome from './pages/FortniteHome';
+import data from './pages/Fortnitedata';
+import Cart from './pages/FortniteCart';
+import Itemcard from './pages/FortniteItemcard';
+import Login from './pages/login';
+import Signup from './pages/Signup';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BalanceProvider>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/Home" element={<Home />} />
+          <Route path="/game" element={<Game />} />
+          <Route path="/add-funds" element={<AddFunds />} />
+          <Route path="/ARKHome" element={<ARKHome />} /> {/* เส้นทางที่นำไปสู่ ARKHome */}
+          <Route path="/dino-pack" element={<DinoPackPage />} />
+          <Route path="/blueprint" element={<BlueprintPage />} />
+          <Route path="/equipment" element={<EquipmentPage />} />
+          <Route path="/artifacts" element={<ArtifactsPage />} />
+          <Route path="/cave" element={<CavePage />} />
+          <Route path="/structures" element={<StructuresPage />} />
+          <Route path="/checkout" element={<CheckoutPage />} />
+          <Route path="/ROVHome" element={<ROVHome />} />
+          <Route path="/ROVShop" element={<ROVShop />} />
+          <Route path="/FortnightHome" element={<FortnightHome />} />
+          <Route path="/data" element={<data />} />
+          <Route path="/Cart" element={<Cart />} />
+          <Route path="/Itemcard" element={<Itemcard />} />
+
+        </Routes>
+      </Router>
+    </BalanceProvider>
   );
 }
 
